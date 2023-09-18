1f68fcc5bf18c3b9be9da270fabb5b2a66fd200b
Mlvwly7 <willymolyvann@gmail.com>
2023-09-18 15:00:23 +0200

    18:24

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Artiste;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker=Factory::create("fr_FR");
+        
+        $fichierArtisteCsv=fopen(__DIR__."/artiste.csv","r");
+        while (!feof($fichierArtisteCsv)) {
+            $lesArtistes[]=fgetcsv($fichierArtisteCsv);
+        }
+        fclose($fichierArtisteCsv);
+
+    $genres=["men", "women"];
+        foreach ($lesArtistes as $value) {
+            $artiste=new Artiste();
+            $artiste    ->setId(intval($value[0]))
+                        ->setNom($value[1])
+                        ->setDescription("<p>". join("</p><p>",$faker->paragraphs(5)) .  "</p>")
+                        ->setSite($faker->url())
+                        ->setImage('https://randomuser.me/api/portraits/'.$faker->randomElement($genres)."/".mt_rand(1,99).".jpg")
+                        ->setType($value[2]);
+            $manager->persist($artiste);
+            }
+            $manager->flush();
+    }
+}
