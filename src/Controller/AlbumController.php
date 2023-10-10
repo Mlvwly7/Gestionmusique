fce91961d87cc533ddab04a38f579ff4a9d2b6ae
Mlvwly7 <willymolyvann@gmail.com>
2023-10-10 12:04:29 +0200

    15:04

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Album;
+use App\Entity\Artiste;
+use App\Repository\AlbumRepository;
+use App\Repository\ArtisteRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AlbumController extends AbstractController
 {
     /**
-     * @Route("/album", name="app_album")
+     * @Route("/albums", name="albums", methods={"GET"})
      */
-    public function index(): Response
+    public function listealbums(AlbumRepository $repo)
     {
-        return $this->render('album/index.html.twig', [
-            'controller_name' => 'AlbumController',
+        $albums=$repo->findAll();
+        return $this->render('album/listeAlbums.html.twig', [
+            'lesAlbums' => $albums
+        ]);
+    }
+
+        /**
+     * @Route("/album/{id}", name="ficheAlbum", methods={"GET"})
+     */
+    public function ficheArtiste(Album $album)
+    {
+        return $this->render('album/ficheAlbum.html.twig', [
+            'leAlbum' => $album
         ]);
     }
 }
